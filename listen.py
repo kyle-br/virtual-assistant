2117153bc37d847b6643406299b08e96eb5d1191
kyle-br <letanky0@gmail.com>
2022-08-08 22:44:31 -0500

    add main.py and listen.py

---
+++ listen.py
@@ -0,0 +1,178 @@
+import speech_recognition as sr
+from datetime import date, datetime
+import pyttsx3   #Convert text to speech (need to install pyttsx3 and pywind32 )
+import pywhatkit #Playing YouTube Videos 
+import requests #weather report for city
+import webbrowser
+import time
+import random
+from word2number import w2n
+# Robot mouth part
+robot_mouth = pyttsx3.init()
+voices=robot_mouth.getProperty('voices')
+robot_mouth.setProperty('voice',voices[1].id)
+robot_mouth.setProperty('rate', 140) # Decrease the Speed Rate x2
+
+def talk(text): #talk function
+    robot_mouth.say(text)
+    robot_mouth.runAndWait()
+
+# Robot ear part
+robot_ear= sr.Recognizer() 
+def take_command():
+    try:
+        with sr.Microphone() as mic: 
+            print("""
+                Hello, my name is ivy, your virtual assistant
+                Make sure to say 'ivy' first before giving a command.
+                -To do math problems, say "ivy calculate (problem)"
+                -To open a website, say "ivy open (name of website)"
+                 """)
+            # talk("""Hello, my name is ivy, your virtual assistant. Make sure to say 'ivy' first before giving a command.
+            #      """)
+            
+            print('listening...')
+            robot_ear.adjust_for_ambient_noise(mic)
+            audio= robot_ear.listen(mic)
+            command=robot_ear.recognize_google(audio)
+            command= command.lower() 
+            if'ivy' in command: 
+                command=command.replace('ivy ',"")
+            else:
+                talk('please make sure say "ivy" first')
+                print('please make sure say "ivy" first')
+                return ""
+    except:
+        you=""
+    return command
+
+def run_alexa(): #this function pass all command to the coresponding methods
+    ans=take_command()
+    print(ans)
+    if ans[0:4]=='play':
+        play(ans)
+
+    if ans[0:9]=='calculate':
+        ans= ans.split(" ")
+        ans.pop(0)
+        calc(ans)
+    elif 'what' in ans:
+        if "today" in ans:
+            today_date()
+        if "time" in ans:
+            time_now()
+        if "weather" in ans :
+            weather()
+    elif "open" in ans:
+        open_websites(ans)
+    elif "how are you" in ans:
+        print("I feel good. Thanks for asking")
+        talk("I feel good. Thanks for asking")
+    elif "how old are you" in ans:
+        print("They say that age is nothing but a number. But technically, it's also a word")
+        talk("They say that age is nothing but a number. But technically, it's also a word")
+    elif "tell me a joke" in ans:
+        jokes()
+    else:
+        print("Sorry, try another command")
+
+
+def play(a): # This function will assign the command and pop up youtube
+    song = a.replace('play','')
+    talk('playing'+ song)
+    print('Now playing:' + song)
+    pywhatkit.playonyt(song) #play on YTube
+    exit()
+
+def today_date(): 
+    today= date.today()
+    talk('Okay, today is: '+ today.strftime("%B %d, %Y"))
+    print('Today is:' + today.strftime("%B %d, %Y"))
+
+def time_now():
+    time= datetime.now()
+    talk('It is '+ time.strftime("%H:%M"))
+    print('Current time: '+ time.strftime("%H:%M:%S"))
+    
+def weather():
+    talk("here's there weather report for today")
+    url= 'https://wttr.in/{}'.format('baton rouge')
+    res= requests.get(url)
+    print(res.text)
+
+
+def calc(agrs): # calculate basic math   
+    if '+' in agrs[1]:
+        agrs=w_to_n(agrs[0]) + w_to_n(agrs[2])
+        agrs= str(agrs)
+        print("Answer: ",agrs)
+        talk("Answer is: "+ agrs)
+    
+    if '-' in agrs[1]:
+        agrs=w_to_n(agrs[0]) - w_to_n(agrs[2])
+        agrs= str(agrs)
+        print("Answer: ",agrs)
+        talk("Answer is: "+ agrs)
+    
+    if '*' in agrs[1]:     
+        agrs=w_to_n(agrs[0]) * w_to_n(agrs[2])
+        agrs= str(agrs)
+        print("Answer: ",agrs)
+        talk("Answer is: "+ agrs)
+   
+    if '/' in agrs[1]:  
+        agrs=w_to_n(agrs[0]) / w_to_n(agrs[2])
+        agrs= str(agrs)
+        print("Answer: ",agrs)
+        talk("Answer is: "+ agrs)
+        
+        
+    
+def w_to_n(x):
+    x= w2n.word_to_num(x)
+    return x
+def open_websites(ans):
+    if 'open youtube' in ans:
+        webbrowser.open_new_tab("https://www.youtube.com")
+        talk("youtube is open now")
+        
+
+    elif 'open google' in ans:
+        webbrowser.open_new_tab("https://www.google.com")
+        talk("Google chrome is open now")
+        
+
+    elif 'open gmail' in ans:
+        webbrowser.open_new_tab("https://www.gmail.com")
+        talk("Google Mail open now")
+        
+    elif 'open facebook' in ans:
+        webbrowser.open_new_tab("https://www.facebook.com")
+        talk("Facebook is open now")
+        
+    elif 'open amazon' in ans:
+        webbrowser.open_new_tab("https://www.amazon.com")
+        talk("Amazon is open now")
+        
+    elif 'open yahoo' in ans:
+        webbrowser.open_new_tab("https://www.yahoo.com")
+        talk("Yahoo is open now")
+    
+    else: 
+        print("That's exceed my ability at this moment")
+        talk("That's exceed my ability at this moment")
+    
+def jokes():
+    jlist = ["What's the best thing about Switzerland? I don't know, but the flag is a big plus.",
+            "Did you hear about the mathematician who's afraid of negative numbers? He'll stop at nothing to avoid them.",
+            "Hear about the new restaurant called Karma? There's no menu: You get what you deserve.",
+            "Did you hear about the actor who fell through the floorboards? He was just going through a stage.",
+            "Where are average things manufactured? The satisfactory.",
+            "How do you drown a hipster? Throw him in the mainstream.",
+            "How many programmers does it take to change a light bulb? None, that's a hardware problem",
+            "Whats the object-oriented way to become wealthy?..Inheritance"
+            "Why did the programmer quit his job? Because he didn't get arrays."]
+    joke=random.choice(jlist)
+    print(joke)
+    talk(joke)
+    
---
+++ main.py
@@ -0,0 +1,16 @@
+from listen import run_alexa
+try:
+    run_alexa()
+except:
+    print("function has ended")
+    
+
+
+# This is a virtual assistant named Ivy    
+# Here's the user guidence  
+#  1.Make sure to say 'ivy' first before giving a command.
+#  2.To do basic math problem , say    'Ivy calculate.......'                 for ex you can say: 'Ivy calculate ten plus four'
+#  3.To play any video on Youtube, say 'Ivy play............'                 for ex you can say: 'Ivy play Roar by Katy Perry'
+#  4.To know the times, date, or weather, say 'Ivy, What....'                 for ex you can say: 'Ivy, what time is it'  or 'Ivy, what's the weather for today'
+#  5.To listen to a joke, say 'Ivy, tell me a joke'
+#  6.To open any web-browers, say 'Ivy, open..(name of website)'
